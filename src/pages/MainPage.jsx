ed2f379b74f0f858cad6a72e172125146bebcf6d
Lee-chaerin <5543705@naver.com>
2022-09-26 22:54:04 +0900

    메인 윗부분 제작

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <div id="main">
-      <div className="main1">사진 배너, 학과 캘린더</div>
-      <div className="main2">아이콘</div>
-      <div className="main3">학과 갤러리</div>
-      <div className="main4">공모전</div>
+      <div className="main1 width">
+        <div>
+          <h6>학과 학사일정</h6>
+        </div>
+      </div>
+      <div className="main2">
+        <Link to={'/'}><img alt='icon' src='icon.png'/>ICON</Link>
+        <Link to={'/'}><img alt='icon' src='icon.png'/>ICON</Link>
+        <Link to={'/'}><img alt='icon' src='icon.png'/>ICON</Link>
+        <Link to={'/'}><img alt='icon' src='icon.png'/>ICON</Link>
+      </div>
+      <div className="main3 width">학과 갤러리</div>
+      <div className="main4 width">공모전</div>
     </div>
   )
 }
