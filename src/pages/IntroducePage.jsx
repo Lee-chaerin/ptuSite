82a5491c417d7d18a362a7b0489ba8561f9945eb
Lee-chaerin <5543705@naver.com>
2022-09-28 22:39:31 +0900

    introduce 분할  범위 수정

--- src/pages/IntroducePage.jsx
+++ src/pages/IntroducePage.jsx
@@ -5,11 +5,11 @@ const IntroducePage = () => {
     <div id="introduce">
       <div className="top">
         <h1>INTRODUCE</h1>
-
-
       </div>
-      <div className="introduce1">왼쪽</div>
-      <div className="introduce2">오른쪽</div>
+      <div>
+        <div className="introduce1">왼쪽</div>
+        <div className="introduce2">오른쪽</div>
+      </div>  
     </div>
   )
 }
