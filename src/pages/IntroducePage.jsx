0b3df9f25b43bdae67d626dc971ec5caceb3d1ed
Lee-chaerin <5543705@naver.com>
2022-09-29 22:36:52 +0900

    introduce 좌측 리스트 복붙

--- src/pages/IntroducePage.jsx
+++ src/pages/IntroducePage.jsx
@@ -7,7 +7,16 @@ const IntroducePage = () => {
         <h1>INTRODUCE</h1>
       </div>
       <div>
-        <div className="introduce1">왼쪽</div>
+        <div className="introduce1">
+          <h1>INTRODUCE</h1>
+          <ul>
+            <li>‣<a href="/introduce/introduce.html">학과 소개</a></li>
+            <li>‣<a href="/introduce/professor.html">전공교수 소개</a></li>
+            <li>‣<a href="/introduce/member.html">임원 소개</a></li>
+            <li>‣<a href="/introduce/notice.html">학과 공지</a></li>
+            <li>‣<a href="/introduce/gallery.html">학과 갤러리</a></li>
+          </ul> 
+        </div>          
         <div className="introduce2">오른쪽</div>
       </div>  
     </div>
